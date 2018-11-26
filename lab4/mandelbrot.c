07b2bf957f561d9a19d15be1f5eaf857d9dc813a
kumarnikhil936 <kumar.nikhil936@gmail.com>
2018-11-26 16:08:10 +0100

    final

--- lab4/mandelbrot.c
+++ lab4/mandelbrot.c
@@ -24,66 +24,30 @@ typedef struct Block_{
 
 
 int checkMandelbrot(float real, float imag, int cutoff){
-/*
-	float x_init = 0;
-	float y_init = 0;
-	float xtemp = 0;
-	float ytemp = 0;
+
+	float x_init = real;
+	float y_init = imag;
+	float xtemp = real;
+	float ytemp = imag;
 	int iter = 0;
 	float x = real;
 	float y = imag;
 
-	while ((x*x + y*y < 2*2) & (iter < cutoff)) {
-		xtemp = x*x - y*y + x_init;
-		ytemp = 2*x*y + y_init;
-		if (x == xtemp & y == ytemp) {
-			iter = cutoff;
-			break;
-		}
+	while (iter < cutoff) {
 		x = xtemp;
 		y = ytemp;
+
+		xtemp = x*x - y*y + x_init;
+		ytemp = 2*x*y + y_init;
+
+		if (xtemp*xtemp + ytemp*ytemp > 4) {
+			return 0;
+		}
+		
 		iter = iter + 1;
 	}
 
-	return iter;
-	*/ 
-	/*
-		Task 2
-		------
-		Implement the code for determining whether the point c (in the complex plane, i.e. c = real + i*imag) is in the Mandelbrot
-                set (https://en.wikipedia.org/wiki/Mandelbrot_set).
-
-		The point (c = real + i*imag) is in the Mandelbrot set if the following sequence of complex numbers z_n is bounded
-                (i.e. |z_n| <= 2  which means  sqrt(Real(z_n)^2 + Imag(z_n)^2) <=2 )
-
-                 z_0 = 0
-                 z_1 = z_0^2 + c
-                 z_2 = z_1^2 + c
-                
-                 ... 
-
-		 i.e. z_{n+1} = (z_n)^2 + c
-
-                Consider that z_n are complex numbers!
-
-                 
-		Perform the iteration up to as many times as the cutoff states, at which point you assume the point is in the set.
-		If the absolute value (|z_n|) is ever greater than 2, the series will diverge for sure and you can conclude that the point is not in the set.
-
-                This function returns the number of iterations n after that sqrt(Re(z_n)^2 + Im(z_n)^2) > 2.
-
-
-		Notes:
-			- You can do this test without calling the expensive square root function.
-
-		Solution:
-			- Looks like the set in Wikipedia.
-	*/
-
-
-	if(real > imag) return cutoff; //<-- This needs to be modified.
-	return 0;                      //<-- This needs to be modified.
-
+	return cutoff;
 }
 
 
@@ -106,24 +70,11 @@ void HandleBlock(int my_rank, Block block, MPI_Win* window, int total_size_x, in
 
 	for(v=0;v<block.target_size;++v){
 
-		 /*
-			Task 1
-			------
-			Transfer the pixels calculated above to the memory exposed by Process 0 using MPI_Put
+		MPI_Win_lock( MPI_LOCK_EXCLUSIVE, 0, 0, *window);
 
-			Notes:
-				- Due to the different layouts, you need to do this line by line
-				- Target location for each line is the base position(block.target_pos) + an offset(v*total_size_x) that depends on the dimensions of the target matrix
-				- Lock and unlock. This causes the write to happen before we overwrite our local results with the next block. (Passive Target Synchronization).
-			Solution:
-				- A triangular shape appears as the output
-		*/
-	MPI_Win_lock( MPI_LOCK_EXCLUSIVE, 0, 0, *window);
+        	MPI_Put( (*my_local_results) + v*block.target_size, block.target_size, MPI_INT, 0, (v * total_size_x) + block.target_pos, block.target_size, MPI_INT, *window);
 
-        MPI_Put( (*my_local_results) + v*block.target_size, block.target_size, MPI_INT, 0, (v * total_size_x) + block.target_pos, block.target_size, MPI_INT, *window);
-
-        MPI_Win_unlock(0, *window);
-		
+      	        MPI_Win_unlock(0, *window);
 	}
 }
 
@@ -202,33 +153,40 @@ int main(int argc, char *argv[]){
 	//See below
 	int * my_result_vals;
 
-	int blocks_per_direction = 1;
+	int blocks_per_direction = total_ranks; // total number of processes
 	int total_blocks = blocks_per_direction * blocks_per_direction;
 	int block_pixel_size = output_size_pixels / blocks_per_direction;
 	//Allocate a block of memory to keep our local results until sending
 	my_result_vals = malloc(sizeof(int)*(block_pixel_size  * block_pixel_size));
 
 
+	int block_coord_x = 0; //In full blocks
+	int block_coord_y = 0;
+	
+	int blocks;
+	for (blocks=0; blocks<blocks_per_direction; blocks++) {
+	//Make a block and fill it with the neccessary information
+	Block block;
 
-	if(my_rank == 1){
-		int block_coord_x = 0; //In full blocks
-		int block_coord_y = 0;
-		//Make a block and fill it with the neccessary information
-		Block block;
-		block.x = pos_x + size / blocks_per_direction * block_coord_x;//upper left corner of the block we want to manage
-		block.y = pos_y + size / blocks_per_direction * block_coord_y;
-		block.size = size / blocks_per_direction;
+	block_coord_y = blocks;
+	if (blocks % 2 == 0)
+		block_coord_x = my_rank;
+	else
+		block_coord_x = blocks_per_direction - my_rank - 1;
 
-		//in pixels
-		block.target_size = block_pixel_size;
-		//Start of the first row in our target matrix.
-		//Second row will start with an offset of +output_size_pixels, etc.
-		block.target_pos = block.target_size * (block_coord_x + block_coord_y * output_size_pixels);
+	block.x = pos_x + size / blocks_per_direction * block_coord_x;//upper left corner of the block we want to manage
+	block.y = pos_y + size / blocks_per_direction * block_coord_y;
+	block.size = size / blocks_per_direction;
+
+	//in pixels
+	block.target_size = output_size_pixels / blocks_per_direction;
+	//Start of the first row in our target matrix.
+	//Second row will start with an offset of +output_size_pixels, etc.
+	block.target_pos = block.target_size * (block_coord_x + block_coord_y * output_size_pixels);
 
 
-		HandleBlock(my_rank,block,&window,output_size_pixels, &my_result_vals, max_number_iterations);
-	}
-
+	HandleBlock(my_rank,block,&window,output_size_pixels, &my_result_vals, max_number_iterations);
+}
 
 	//Before outputting the result we wait for all the values
 	MPI_Barrier(MPI_COMM_WORLD);
@@ -239,7 +197,7 @@ int main(int argc, char *argv[]){
 
 	if(my_rank == 0){
 		char filename[100];
-		sprintf(filename,"Mandelbrot_x%f y%f size %f.ppm",pos_x,pos_y,size);
+		sprintf(filename,"Mandelbrot_x%f_y%f_size%f.ppm",pos_x,pos_y,size);
 		FILE *fp = fopen(filename, "wb"); /* b - binary mode */
 		fprintf(fp, "P6\n%d %d\n255\n", output_size_pixels,output_size_pixels);
 
