f52cf7e9f26342fc59e007a61ababe58de6f9780
kumarnikhil936 <kumar.nikhil936@gmail.com>
2019-01-14 00:09:51 +0100

    final commit

---
+++ lab8/StateManager.cpp
@@ -0,0 +1,49 @@
+#include "StateManager.h"
+
+#include <mutex>
+#include <algorithm>
+
+using namespace std;
+
+void StateManager::enterSolution(const State& res){
+	if(res.solutionSize() < best_solution_size){
+	        omp_set_lock(&lck1);
+
+		printf("Solution updated, size %d \n",res.solutionSize());
+		best_solution = res;
+		best_solution_size = res.solutionSize();
+	
+	        omp_unset_lock(&lck1);
+
+	}
+}
+
+bool StateManager::claim(const State& val){
+		
+
+	/*
+		If we find that some other task has already claimed the exact configuration and reached it in as many or less steps,
+		we return false so it won't be worked on anymore.
+	*/
+	auto val_it = state_set.find(val);
+	if(val_it != state_set.cend() && val.solutionSize()>=val_it->solutionSize()){
+		return false; //Someone else is already doing that / has already done that
+	}
+
+	//Two val elements are treated as equal, even if the steps to reach them are different. 
+	//Erasing and re-inserting updates that value
+	omp_set_lock(&lck2);
+
+	state_set.erase(val);
+	state_set.insert(val);	
+	
+	omp_unset_lock(&lck2);
+
+	return true;
+};
+
+
+void StateManager::printBestSolution(){
+	best_solution.printSolution();
+}
+
---
+++ lab8/lab8-script.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+module load compiler/GCC/8.2.0-2.31.1
+make compile run
