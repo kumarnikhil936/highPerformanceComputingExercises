7692242470052a77915d9db80b2e9fe46297f22b
kumarnikhil936 <kumar.nikhil936@gmail.com>
2019-01-22 22:07:46 +0100

    final commit

--- lab9/conjGrad.cpp
+++ lab9/conjGrad.cpp
@@ -16,12 +16,12 @@ using matrix = vector<vec>;
 
 vec conjGrad( const matrix &A, const vec &B )
 {
-    int n = A.size();
+    uint n = A.size();
     vec X( n, 0.0 );
     
     vec R = B;
     vec P = R;
-    int k = 0, i = 0;
+    uint k = 0, i = 0;
     
 #pragma omp parallel
 {
@@ -33,7 +33,6 @@ vec conjGrad( const matrix &A, const vec &B )
         #pragma omp parallel for
         for (i = 0; i < A.size(); i++) AP[i] = (inner_product(A[i].begin(), A[i].end(), P.begin(), 0.0));
         
-        #pragma omp parallel for
         double alpha = (inner_product(R.begin(), R.end(), R.begin(), 0.0)) / max( (inner_product(R.begin(), R.end(), AP.begin(), 0.0)), tolerance );
         
         #pragma omp parallel for
@@ -63,7 +62,7 @@ vec conjGrad( const matrix &A, const vec &B )
 int main(int argc, char *argv[])
 {
     double d, sigma, s;
-    int i, j, n;
+    uint i, j, n;
     double temp = 0.0;
     double max_err = 0.0;
 
@@ -106,7 +105,6 @@ int main(int argc, char *argv[])
     }
 
     // Create the vector B from the equation given in sheet
-    #pragma omp parallel for
     for (i=0; i<n; i++) {
         for (j=0; j<n; j++) {
             temp += A[i][j]*sol[j];
@@ -117,7 +115,6 @@ int main(int argc, char *argv[])
     vec X = conjGrad( A, B );
     
     cout << "\nSolving for Ax = B \n\nMatrix A = \n";
-    #pragma omp parallel for
     for ( i = 0; i < A.size(); i++ )
     {
         for ( j = 0; j < A[0].size(); j++ )
@@ -160,7 +157,7 @@ int main(int argc, char *argv[])
     
     cout << "\nMax error between X and sol is " << max_err << "\n";
     
-    cout << "\nTotal Time to solve = %.9f seconds\n" << double(end-start)/CLOCKS_PER_SEC << "\n";
+    cout << "\nTotal Time to solve " << double(end-start)/CLOCKS_PER_SEC << "seconds \n";
 }
 
 
